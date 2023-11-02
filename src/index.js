7713bc502e4f8b87dac2fc5731ce5000d497f300
tttom3669 <tttom3669@gmail.com>
2023-11-02 21:35:58 +0800

    git init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const a = 1;
+
+console.log(a,PRODUCTION);
+
+import './all.css';
+
+class Test {
+    #a = 100;
+}
+
+const tt = new Test();
+
+console.log('tt',tt.a);
+
+[1, 2, 3].map(n => n + 1);
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path'); // 指向實體路徑位置
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i, // 正則表達式，當看到 css 副檔名時使用的 loader
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(png|gif)/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[hash].css',
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: "./static", to: "./static" },
+    //   ],
+    // }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(false),
+    }),
+    new CompressionPlugin()
+  ],
+  devtool: 'source-map',
+};
